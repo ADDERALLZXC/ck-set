7a69b7816fe6b33e6f4aa3ca8097483235b93b62
Alexander Medvedev <86624245+ADDERALLZXC@users.noreply.github.com>
2021-09-16 22:32:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const sizeLabel = document.querySelectorAll(".label");
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const clickPopUpInner = document.querySelector('.oneclick-popup__inner');
+const optionsList = document.querySelectorAll(".option");
 
-function labelToggle() {
-	sizeLabel.classList.toggle("label");
-}
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+  clickPopUpInner.classList.toggle("oneclick-popup__inner-active")
+});
 
-sizeLabel.forEach((eq) => {
-    eq.addEventListener('click', labelToggle)
-})
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+	clickPopUpInner.classList.remove("oneclick-popup__inner-active")
+  });
+});
